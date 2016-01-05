3cf911cc1db49aee9559a652e8763bddfb8c6a2e
Holger Benl <hbenl@evandor.de>
2016-01-05 11:43:17 +0100

    re-worked the debug adapter, now it should handle sources and breakpoints correctly

--- src/firefoxDebug.ts
+++ src/firefoxDebug.ts
@@ -1,16 +1,15 @@
-import { DebugSession, InitializedEvent, TerminatedEvent, StoppedEvent, OutputEvent, ThreadEvent, Thread, StackFrame, Scope, Source} from './vscode/debugSession';
-import { TabActorProxy } from './mozilla/actorProxy';
+import { DebugSession, InitializedEvent, TerminatedEvent, StoppedEvent, OutputEvent, ThreadEvent, Thread, StackFrame, Scope, Source} from 'vscode-debugadapter';
+import { ActorProxy, TabActorProxy, ThreadActorProxy, SourceActorProxy, BreakpointActorProxy } from './mozilla/actorProxy';
 import { MozDebugConnection } from './mozilla/mozDebugConnection';
 
 class FirefoxDebugSession extends DebugSession {
 
 	private mozDebugConnection: MozDebugConnection;
+
 	private nextThreadId = 1;
-	
-	// map threadIds to tabActors
-	private tabs = new Map<number, TabActorProxy>();
-	// map tabActor names to threadIds
-	private tabThreadIds = new Map<string, number>();
+	private threadsById = new Map<number, ThreadInfo>();
+	private threadsByActorName = new Map<string, ThreadInfo>();
+	private breakpointsBySourceUrl = new Map<string, DebugProtocol.SetBreakpointsArguments>();
 	
 	public constructor(debuggerLinesStartAt1: boolean, isServer: boolean = false) {
 		super(debuggerLinesStartAt1, isServer);
@@ -22,20 +21,37 @@ class FirefoxDebugSession extends DebugSession {
 		// connect to Firefox
 		this.mozDebugConnection = new MozDebugConnection();
 
+		// attach to all tabs, register the corresponding threads
+		// and inform VSCode about them
 		this.mozDebugConnection.rootActor.onTabOpened((tabActor) => {
-			let threadId = this.nextThreadId++;
-			this.tabs.set(threadId, tabActor);
-			this.tabThreadIds.set(tabActor.name, threadId);
-			this.sendEvent(new ThreadEvent('started', threadId));
+			tabActor.attach().then((threadActor) => {
+
+				let threadId = this.nextThreadId++;
+				let threadInfo = new ThreadInfo(threadId, threadActor);
+				this.threadsById.set(threadId, threadInfo);
+				this.threadsByActorName.set(threadActor.name, threadInfo);
+
+				threadActor.onNewSource((sourceActor) => {
+					let sourceInfo = new SourceInfo(sourceActor);
+					threadInfo.sources.push(sourceInfo);
+					if (this.breakpointsBySourceUrl.has(sourceActor.url)) {
+						let breakpoints = this.breakpointsBySourceUrl.get(sourceActor.url).lines;
+						this.setBreakpointsOnSourceActor(breakpoints, sourceInfo, threadActor);
+					}
+				});
+				
+				threadActor.onExited(() => {
+					this.threadsById.delete(threadId);
+					this.threadsByActorName.delete(threadActor.name);
+					this.sendEvent(new ThreadEvent('exited', threadId));
+				});
+				
+				threadActor.fetchSources();
+
+				this.sendEvent(new ThreadEvent('started', threadId));
+			});
 		});
 
-		this.mozDebugConnection.rootActor.onTabClosed((tabActor) => {
-			let threadId = this.tabThreadIds.get(tabActor.name);
-			this.tabs.delete(threadId);
-			this.tabThreadIds.delete(tabActor.name);
-			this.sendEvent(new ThreadEvent('exited', threadId));
-		})
-		
 		this.mozDebugConnection.rootActor.fetchTabs();
 		
 		// now we are ready to accept breakpoints -> fire the initialized event to give UI a chance to set breakpoints
@@ -43,9 +59,125 @@ class FirefoxDebugSession extends DebugSession {
 	}
 
 	protected threadsRequest(response: DebugProtocol.ThreadsResponse): void {
-		// initially, we return no threads - all threads will be registered later
-		response.body = { threads: [] };
+
+		let responseThreads: Thread[] = [];
+		this.threadsById.forEach((threadInfo) => {
+			responseThreads.push(new Thread(threadInfo.id, threadInfo.actor.name));
+		});
+		response.body = { threads: responseThreads };
+		
 		this.sendResponse(response);
+	}
+	
+    protected setBreakPointsRequest(response: DebugProtocol.SetBreakpointsResponse, args: DebugProtocol.SetBreakpointsArguments): void {
+
+		let firefoxSourceUrl = this.convertDebuggerPathToClient(args.source.path);
+		this.breakpointsBySourceUrl.set(firefoxSourceUrl, args);
+
+		let responseSent = false;		
+		this.threadsById.forEach((threadInfo) => {
+			
+			let sourceInfo: SourceInfo = null;
+			for (let i = 0; i < threadInfo.sources.length; i++) {
+				if (threadInfo.sources[i].actor.url === firefoxSourceUrl) {
+					sourceInfo = threadInfo.sources[i];
+					break;
+				}
+			}
+
+			if (sourceInfo !== null) {
+				let setBreakpointsPromise = this.setBreakpointsOnSourceActor(args.lines, sourceInfo, threadInfo.actor);
+				if (!responseSent) {
+					setBreakpointsPromise.then((breakpointInfos) => {
+						response.body.breakpoints = breakpointInfos.map((breakpointInfo) => 
+							<DebugProtocol.Breakpoint>{ verified: true, line: breakpointInfo.actualLine });
+					});
+					responseSent = true;
+				}
+			}
+		});
+	}
+	
+	private setBreakpointsOnSourceActor(breakpointsToSet: number[], sourceInfo: SourceInfo, threadActor: ThreadActorProxy): Promise<BreakpointInfo[]> {
+
+		if (threadActor.isKnownToBePaused) {
+			return this.setBreakpointsOnPausedSourceActor(breakpointsToSet, sourceInfo);
+		} else {
+			return threadActor.interrupt().then(() => this.setBreakpointsOnPausedSourceActor(breakpointsToSet, sourceInfo));
+		}
+	}
+
+	private setBreakpointsOnPausedSourceActor(breakpointsToSet: number[], sourceInfo: SourceInfo): Promise<BreakpointInfo[]> {
+		
+		let result = new Promise<BreakpointInfo[]>((resolve) => {
+			sourceInfo.currentBreakpoints.then((oldBreakpoints) => {
+				
+				let newBreakpoints: BreakpointInfo[] = [];
+				let breakpointsBeingRemoved: Promise<void>[] = [];
+				let breakpointsBeingSet: Promise<void>[] = [];
+				
+				oldBreakpoints.forEach((breakpointInfo) => {
+					let breakpointIndex = breakpointsToSet.indexOf(breakpointInfo.requestedLine);
+					if (breakpointIndex >= 0) {
+						newBreakpoints[breakpointIndex] = breakpointInfo;
+						breakpointsToSet[breakpointIndex] = undefined;
+					} else {
+						breakpointsBeingRemoved.push(breakpointInfo.actor.delete());
+					}
+				});
+
+				breakpointsToSet.map((requestedLine, index) => {
+					if (requestedLine !== undefined) {
+						breakpointsBeingSet.push(sourceInfo.actor.setBreakpoint({ line: requestedLine })
+						.then((setBreakpointResult) => {
+							newBreakpoints[index] = new BreakpointInfo(
+								requestedLine, setBreakpointResult.actualLocation.line, setBreakpointResult.breakpointActor); 
+						}));
+					}
+				});
+				
+				Promise.all(breakpointsBeingRemoved).then(() => Promise.all(breakpointsBeingSet)).then(() => {
+					resolve(newBreakpoints);
+				});
+			});
+		});
+		
+		sourceInfo.currentBreakpoints = result;
+		return result;
+	}
+}
+
+class ThreadInfo {
+	public id: number;
+	public actor: ThreadActorProxy;
+	public sources: SourceInfo[];
+	
+	public constructor(id: number, actor: ThreadActorProxy) {
+		this.id = id;
+		this.actor = actor;
+		this.sources = [];
+	}
+}
+
+class SourceInfo {
+	public actor: SourceActorProxy;
+	public currentBreakpoints: Promise<BreakpointInfo[]>;
+	
+	public constructor(actor: SourceActorProxy) {
+		this.actor = actor;
+		this.currentBreakpoints = Promise.resolve([]);
+	}
+}
+
+class BreakpointInfo {
+	public requestedLine: number;
+	public actualLine: number;
+	public actor: BreakpointActorProxy;
+	
+	public constructor(requestedLine: number, actualLine: number, actor: BreakpointActorProxy) {
+		this.requestedLine = requestedLine;
+		this.actualLine = actualLine;
+		this.actor = actor;
 	}
 }
 
