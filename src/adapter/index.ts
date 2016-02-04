f5597c8fd1739178ab24667f9506b383ccd8cee8
Holger Benl <hbenl@evandor.de>
2016-02-04 14:08:02 +0100

    move FrameAdapter to a separate file

--- src/adapter/index.ts
+++ src/adapter/index.ts
@@ -1,3 +1,4 @@
-export { ThreadAdapter, SourceAdapter, BreakpointAdapter, FrameAdapter } from './misc';
+export { ThreadAdapter, SourceAdapter, BreakpointAdapter } from './misc';
+export { FrameAdapter } from './frame';
 export { EnvironmentAdapter, ObjectEnvironmentAdapter, FunctionEnvironmentAdapter, WithEnvironmentAdapter, BlockEnvironmentAdapter } from './environment';
 export { VariablesProvider, ScopeAdapter, ObjectScopeAdapter, LocalVariablesScopeAdapter, FunctionScopeAdapter } from './scope';
