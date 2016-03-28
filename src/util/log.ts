cc841779ba48e8fa92e76bdfe24b83a5d2e7d0fe
Holger Benl <hbenl@evandor.de>
2016-03-28 11:23:00 +0200

    add file logging

--- src/util/log.ts
+++ src/util/log.ts
@@ -1,54 +1,88 @@
+import * as fs from 'fs';
+
 export enum LogLevel { Debug, Info, Warn, Error }
 
 export interface LogConfiguration {
-	[logName: string]: LogLevel;
+	fileName?: string;
+	fileLevel?: { [logName: string]: LogLevel };
+	consoleLevel?: { [logName: string]: LogLevel };
 }
 
 export class Log {
 
 	private static startTime = Date.now();
 	
-	private static _config: LogConfiguration = {
-		'default': LogLevel.Info
-	};
+	private static _config: LogConfiguration;
 	
 	private static logs = new Map<string, Log>();
-	
+	private static fileDescriptor: number;
+
 	public static set config(newConfig: LogConfiguration) {
+		if (Log.fileDescriptor !== undefined) {
+			fs.closeSync(Log.fileDescriptor);
+			Log.fileDescriptor = undefined;
+		}
+
 		Log._config = newConfig;
+		if (Log._config.fileName) {
+			try {
+				Log.fileDescriptor = fs.openSync(Log._config.fileName, 'w');
+			} catch(e) {}
+		}
+
 		Log.logs.forEach((log) => log.configure());
-	} 
+	}
 	
 	public static create(name: string): Log {
 		return new Log(name);
 	}
 
-	private logLevel: LogLevel;
+	private fileLevel: LogLevel;
+	private consoleLevel: LogLevel;
+	private minLevel: LogLevel;
 	
 	constructor(private name: string) {
 		this.configure();
 	}
 
 	private configure() {
-		this.logLevel = Log._config[this.name];
-		if (this.logLevel === undefined) {
-			this.logLevel = Log._config['default'];
+		this.fileLevel = undefined;
+		if (Log._config.fileName) {
+			this.fileLevel = Log._config.fileLevel[this.name];
+			if (this.fileLevel === undefined) {
+				this.fileLevel = Log._config.fileLevel['default'];
+			}
+			if (this.fileLevel === undefined) {
+				this.fileLevel = LogLevel.Info;
+			}
 		}
-		if (this.logLevel === undefined) {
-			this.logLevel = LogLevel.Info;
+		this.consoleLevel = Log._config.consoleLevel[this.name];
+		if (this.consoleLevel === undefined) {
+			this.consoleLevel = Log._config.consoleLevel['default'];
+		}
+
+		this.minLevel = this.fileLevel;
+		if (this.consoleLevel && !(this.consoleLevel >= this.minLevel)) {
+			this.minLevel = this.consoleLevel;
 		}
 	}
 	
 	private log(msg: string, level: LogLevel, displayLevel: string) {
-		if (level >= this.logLevel) {
+		if (level >= this.minLevel) {
 			
 			let elapsedTime = (Date.now() - Log.startTime) / 1000;
 			let elapsedTimeString = elapsedTime.toFixed(3);
 			while (elapsedTimeString.length < 7) {
 				elapsedTimeString = '0' + elapsedTimeString;
 			}
+			let logMsg = displayLevel + '|' + elapsedTimeString + '|' + this.name + ': ' + msg;
 			
-			console.log(displayLevel + '|' + elapsedTimeString + '|' + this.name + ': ' + msg);
+			if ((Log.fileDescriptor !== undefined) && (level >= this.fileLevel)) {
+				fs.write(Log.fileDescriptor, logMsg + '\n');
+			}
+			if (level >= this.consoleLevel) {
+				console.log(logMsg);
+			}
 		}
 	}
 
@@ -68,3 +102,14 @@ export class Log {
 		this.log(msg, LogLevel.Error, 'ERROR');
 	}
 }
+
+Log.config = {
+//	fileName: '/tmp/vscode-firefox-debug.log',
+//	fileLevel: {
+//		'default': LogLevel.Debug
+//	},
+	consoleLevel: {
+		'default': LogLevel.Info,
+//		'DebugConnection': LogLevel.Debug,
+	}
+};
