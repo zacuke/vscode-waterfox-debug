f62813a17351aebe43883cdc8218c5290d43205a
Holger Benl <hbenl@evandor.de>
2019-09-07 12:15:22 +0200

    workaround for VS Code resolving ${workspaceFolder} in the debug configuration to a path that is only valid on the remote machine

--- src/extension/debugConfigurationProvider.ts
+++ src/extension/debugConfigurationProvider.ts
@@ -2,11 +2,19 @@ import * as vscode from 'vscode';
 
 export class DebugConfigurationProvider implements vscode.DebugConfigurationProvider {
 
+	/**
+	 * this method is called by VS Code before a debug session is started and makes modifications
+	 * to the debug configuration:
+	 * - some values can be overridden by corresponding VS Code settings
+	 * - when running in a remote workspace, we resolve `${workspaceFolder}` ourselves because
+	 *   VS Code resolves it to a local path in the remote workspace but we need the remote URI instead
+	 */
 	resolveDebugConfiguration(
 		folder: vscode.WorkspaceFolder | undefined,
 		debugConfiguration: vscode.DebugConfiguration
 	): vscode.DebugConfiguration {
 
+		debugConfiguration = { ...debugConfiguration };
 		const settings = vscode.workspace.getConfiguration('firefox', folder ? folder.uri : null);
 
 		const executable = this.getSetting<string>(settings, 'executable');
@@ -34,9 +42,35 @@ export class DebugConfigurationProvider implements vscode.DebugConfigurationProv
 			debugConfiguration.keepProfileChanges = keepProfileChanges;
 		}
 
+		if (folder && (folder.uri.scheme === 'vscode-remote')) {
+
+			const uri = folder.uri.toString();
+			if (debugConfiguration.webRoot) {
+				debugConfiguration.webRoot = debugConfiguration.webRoot.replace('${workspaceFolder}', uri);
+			}
+
+			if (debugConfiguration.pathMappings) {
+
+				const resolvedPathMappings: { url: string, path: string | null }[] = [];
+
+				for (const pathMapping of debugConfiguration.pathMappings) {
+					resolvedPathMappings.push({
+						url: pathMapping.url,
+						path: pathMapping.path.replace('${workspaceFolder}', uri)
+					});
+				}
+
+				debugConfiguration.pathMappings = resolvedPathMappings;
+			}
+		}
+
 		return debugConfiguration;
 	}
 
+	/**
+	 * read a value from the user's VS Code settings. If the user hasn't set a value, this
+	 * method returns `undefined` (instead of the default value for the given key).
+	 */
 	private getSetting<T>(settings: vscode.WorkspaceConfiguration, key: string): T | undefined {
 
 		const values = settings.inspect<T>(key);
