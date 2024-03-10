7859345a1bebe85915d6b9ca3809018edde7615a
Holger Benl <hbenl@evandor.de>
2024-03-10 11:57:13 +0100

    Fix webpack sourcemap tests

--- src/test/testWebpackSourceMaps.ts
+++ src/test/testWebpackSourceMaps.ts
@@ -6,6 +6,7 @@ import * as assert from 'assert';
 import * as util from './util';
 import * as sourceMapUtil from './sourceMapUtil';
 import webpack from 'webpack';
+import TerserPlugin from 'terser-webpack-plugin';
 import { DebugClient } from 'vscode-debugadapter-testsupport';
 
 const TESTDATA_PATH = path.join(__dirname, '../../testdata/web/sourceMaps/modules');
@@ -26,24 +27,21 @@ describe('Webpack sourcemaps: The debugger', function() {
 		}
 	});
 
-	for (let devtool of [
-		'cheap-eval-source-map', 'cheap-source-map', 'cheap-module-eval-source-map', 'inline-source-map',
-		'cheap-module-source-map' , 'eval-source-map' , 'source-map' , 'nosources-source-map'
-	]) {
+	for (const devtool of [
+		'source-map', 'inline-source-map', 'nosources-source-map', 'inline-nosources-source-map',
+		'eval-source-map', 'eval-cheap-source-map', 'eval-cheap-module-source-map',
+		'eval-nosources-source-map', 'eval-nosources-cheap-source-map', 'eval-nosources-cheap-module-source-map',
+	] satisfies Devtool[]) {
 
 		const description = `should map webpack-bundled modules with devtool "${devtool}" to their original sources`;
-
-		// disable tests with webpack devtools that are known to be broken (webpack bug #5491)
-		if (devtool.indexOf('eval') < 0) {
-			it.skip(description);
-			continue;
-		}
+		const isEvalSourcemap = devtool.indexOf('eval') >= 0;
+		const isCheapSourcemap = devtool.indexOf('cheap') >= 0;
 
 		it(description, async function() {
 
 			let targetDir = await prepareTargetDir();
 
-			await build(targetDir, <Devtool>devtool);
+			await build(targetDir, devtool);
 
 			dc = await util.initDebugClient('', true, {
 				file: path.join(targetDir, 'index.html'),
@@ -51,18 +49,19 @@ describe('Webpack sourcemaps: The debugger', function() {
 			});
 
 			// test breakpoint locations if the devtool provides column breakpoints
-			if ((devtool.indexOf('cheap') < 0) && (devtool.indexOf('source-map') >= 0)) {
+			if (!isCheapSourcemap) {
 				const breakpointLocations = await dc.customRequest('breakpointLocations', {
 					source: { path: path.join(targetDir, 'f.js') },
 					line: 7
 				});
 				assert.deepStrictEqual(breakpointLocations.body.breakpoints, [
-					{ line: 7, column: 1 },
+					{ line: 7, column: isEvalSourcemap ? 1 : 2 },
 					{ line: 7, column: 6 }
 				]);
 			}
 
-			await sourceMapUtil.testSourcemaps(dc, targetDir, 1);
+			const breakpoint = { line: 7, column: isEvalSourcemap ? 1 : 6 };
+			await sourceMapUtil.testSourcemaps(dc, targetDir, breakpoint);
 		});
 	}
 });
@@ -76,9 +75,7 @@ async function prepareTargetDir(): Promise<string> {
 	return targetDir;
 }
 
-type Devtool = 
-	'cheap-eval-source-map' | 'cheap-source-map' | 'cheap-module-eval-source-map' | 'inline-source-map' |
-	'cheap-module-source-map' | 'eval-source-map' | 'source-map' | 'nosources-source-map';
+type Devtool = `${'inline-' | 'hidden-' | 'eval-' | ''}${'nosources-' | ''}${'cheap-module-' | 'cheap-' | ''}source-map`;
 
 function build(targetDir: string, devtool: Devtool): Promise<void> {
 	return new Promise<void>((resolve, reject) => {
@@ -89,7 +86,10 @@ function build(targetDir: string, devtool: Devtool): Promise<void> {
 				path: targetDir,
 				filename: 'bundle.js'
 			},
-			devtool: devtool
+			devtool: devtool,
+			optimization: {
+				minimizer: [new TerserPlugin({ terserOptions: { mangle: false } })],
+			}
 		}, (err, stats) => {
 			if (err) {
 				reject(err);
